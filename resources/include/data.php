2e5d232aa957d27e80322c0f633f1ce3cd89bc0c
drewmcd621 <drewmcd621@bitbucket.org>
2013-11-01 12:51:11 -0400

    added sanitize function

--- resources/include/data.php
+++ resources/include/data.php
@@ -1,5 +1,13 @@
 <?php
-
 require_once("pw.php");
 
+function sanitize($input)
+{
+	//Remove semicolons
+	$input = str_replace(";","",$input);
+	//Escapes harmful characters
+	$input = mysqli_real_escape_string($input);
+	
+	return $input;
+}
 ?>
