016f3a6cd17573410f3803d899fb862ddbdd0557
drewmcd621 <drewmcd621@bitbucket.org>
2013-11-01 18:35:28 -0400

    added createUser

--- resources/include/data.php
+++ resources/include/data.php
@@ -18,8 +18,39 @@ function sanitize($input)
 function salthash($text)
 {
 		$salt = "thecaptain";
-		$text = $salt + $text + $salt;
+		$text = $salt . $text . $salt;
 		return sha1($text);
 	
+}
+
+function createUser($username, $password, $email)
+{
+	$sql = getSQL(TRUE);
+	
+	$user = sanitize($username);
+	$pass = salthash($password);
+	$email = sanitize($email);
+	$act_code = satlhash($email);
+	
+	$query = "INSERT INTO e_users (u_activecode, u_name, u_email, u_pass) VALUES ('$act_code','$user','$email','$pass')";
+	
+	mysqli_query($sql,$query) or die(mysqli_error($sql) . ": " .  $query);
+	
+	$query = "SELECT u_id FROM e_users WHERE u_name = '$user'";
+	
+	$res = mysqli_query($sql,$query) or die(mysqli_error($sql) . ": " .  $query);
+	
+			
+	$row = $res->fetch_assoc();
+	
+	if($row)
+	{
+		return $row["u_id"];
+	}
+	else
+	{
+		return -1;
+	}
+
 }
 ?>
