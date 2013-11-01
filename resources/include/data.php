e5940acd30b0a98ec9057c501bf17a8def29cbf4
drewmcd621 <drewmcd621@bitbucket.org>
2013-11-01 15:31:32 -0400

    added salthash function

--- resources/include/data.php
+++ resources/include/data.php
@@ -1,13 +1,25 @@
 <?php
 require_once("pw.php");
 
+
+
 function sanitize($input)
 {
 	//Remove semicolons
 	$input = str_replace(";","",$input);
 	//Escapes harmful characters
 	$input = mysqli_real_escape_string($input);
+	//Escapes % and _
+	$input = addcslashes($input, '%_');
 	
 	return $input;
+}
+
+function salthash($text)
+{
+		$salt = "thecaptain";
+		$text = $salt + $text + $salt;
+		return sha1($text);
+	
 }
 ?>
