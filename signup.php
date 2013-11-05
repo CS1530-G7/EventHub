29070af4b7560b0cd4a233887cacfc7055a675aa
chris lloyd <cl71@chrislloyd.co>
2013-11-05 01:38:16 -0500

    fixed signup page

--- signup.php
+++ signup.php
@@ -9,7 +9,10 @@
 		<!-- <img src="./resources/img/Homepage.jpg" /> -->
 		<form name="signup" action="" method="POST">
 			Username: <input type="text" name="user">
-			FIrst Name: <input type="text" name="first_name">
+			Email: <input type="text" name="email">
+			Password: <input type="password" name="password">
+			Confirm Password: <input type="password" name="password_confirm">
+			First Name: <input type="text" name="first_name">
 			Last Name: <input type="text" name="last_name">
 			<input type="submit" value="Submit">
 		</form>
