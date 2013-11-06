3fabca4cd9587e945f8ae3c5f42eb59ca3e74b6f
chris lloyd <cl71@chrislloyd.co>
2013-11-06 14:23:00 -0500

    add createEvent link

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 		<h1>Coming Soon!</h1>
 		
 		<!-- <img src="./resources/img/Homepage.jpg" /> -->
-		<a href="signup.php">SIGN UP!!!!!!!!</a>
+		<a href="signup.php">SIGN UP!</a>
+		<a href="createEvent.php">SIGN UP!</a>
 	
 		
 	</body>
