3d7933eee0c570365af64693be664910786a36c7
Chris Lloyd <cl71@chrislloyd.co>
2013-11-07 14:37:46 -0500

    ghetto login works

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
-<?php 
+<?php
 
- ?>
+require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/data.php");
+
+?>
  
  <html>
 	<body>
-		<h1>Coming Soon!</h1>
+		<h1>Coming Soon!!</h1>
 		
 		<!-- <img src="./resources/img/Homepage.jpg" /> -->
 		<a href="signup.php">SIGN UP!</a>
 		</br>
 		<a href="createEvent.php">CREATE EVENT!</a>
+		</br>
+		<a href="login.php">LOG IN!</a>
 	
 		
 	</body>
