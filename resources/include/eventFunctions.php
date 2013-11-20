5eece4ccc2872a7fab3dcb2e7817121f62e57152
drewmcd621 <drewmcd621@bitbucket.org>
2013-11-20 00:31:26 -0500

    resolved conflict

--- resources/include/eventFunctions.php
+++ resources/include/eventFunctions.php
@@ -3,21 +3,6 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/data.php");
 
 
-//event search functions here
-function doEventSearch () {
-
-	if(isset($_POST['search_submit'])) {
-
-		$input = $_POST['search_query'];
-
-		$search_results = eventSearch($input, -1, 0, 0, TRUE, FALSE);
-
-		foreach ($search_results as $result){
-			print "<p>{$result}</p>";
-		}
-}
-
-}
 
 function displayEventCard($EID, $Distance=-1)
 {
