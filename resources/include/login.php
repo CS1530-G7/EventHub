c30ed4ef5e3f37385dcdccf77b7ca6f7ca6dce63
Alex Quinn <ajq5623@gmail.com>
2013-11-20 15:14:48 -0500

    Style

--- resources/include/login.php
+++ resources/include/login.php
@@ -60,10 +60,8 @@ $str = <<<END
 	$msg
 </div>
 <form name="login" id="login" action="index.php" method="POST">
-	<label for="username"/>Username:</label>
-	<input type="text" name="username">
-	<label for="password"/>Password:</label>
-	<input type="password" name="password">
+	<input type="text" name="username" value="Username">
+	<input type="password" name="password" value="Password">
 	<input class= "btn" name="submit" type="submit" value="Login">
 </form>
 
