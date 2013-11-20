15559f63809f67ac7f1f4d965a86614c5eb3608d
Alex Quinn <ajq5623@gmail.com>
2013-11-20 13:30:38 -0500

    style

--- index.php
+++ index.php
@@ -10,30 +10,29 @@ $msg = doLogin();
 <html>
 	<head>
  		<title>EventHub</title>
- 		<!-- Time stamp on css to stop caching so I can actually get work done when making changes -->
-        <link rel="stylesheet" type="text/css" href="css/style.css?<?php echo date('l jS \of F Y h:i:s A'); ?>" media="screen" />
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
 	 	<script>
 	 		function RemoveText(obj) {   obj.value = ''; } 
 	 	</script>
     </head>
 
 	<body>
-		<header>
+		<div id="header">
+			<?php login_div($msg); ?>
+		</div>
+	 	<div id="main-center">
+			<!-- Simple login logic here:
+						If not logged in, show the login form
+						If logged in, then welcome user and show profile link
+			-->
+
+			<h1>EventHub</h1>
+
 			<form name="login" id="login" action="index.php" method="POST">
 				<label for="username"/></label>
 				<input type="text" name="search_query" value="" onclick="RemoveText(this);">
 				<input class= "btn" name="search_submit" type="submit" value="Search Events">
 			</form>
-		</header>
-	 	<div id="main_center">
-			<!-- Simple login logic here:
-						If not logged in, show the login form
-						If logged in, then welcome user and show profile link
-			-->
-			<?php login_div($msg); ?>
-
-			<h1>EventHub</h1>
-
 			<div id="search-area">
 				<div id="search_results">
 					<?php doEventSearch(); ?>
