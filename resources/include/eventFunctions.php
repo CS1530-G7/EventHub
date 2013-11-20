0392ec54fca546ea0a67e23af67354b426c86cd1
drewmcd621 <drewmcd621@bitbucket.org>
2013-11-20 00:47:07 -0500

    Added distance to event card

--- resources/include/eventFunctions.php
+++ resources/include/eventFunctions.php
@@ -10,12 +10,19 @@ function displayEventCard($EID, $Distance=-1)
 	$ename = $event["Name"];
 	$edate = date("F j, Y g:i a",strtotime($event["Date"]));
 	$eloc = $event["Location"];
+	$edist = number_format($Distance,2);
 	
 	print "<div id='event-$EID' class='event-card'>
 			<p id='name'><a href='event.php?e=$EID'>$ename</a></p><br>
 			<p id='date'>$edate</p><br>
-			<p id='loc'>$eloc</p>
-			</div>";
+			<p id='loc'>$eloc</p>"
+			if($Distance >= 0)
+			{
+				print "<p id='dist'>$edist</p>";
+			}
+			
+			
+			print "</div>";
 }
 
 
