ee4a0c14c74625e16c84cc3162a2b55de8e0f2c6
Alex Quinn <ajq5623@gmail.com>
2013-11-20 13:27:54 -0500

    style

--- index.php
+++ index.php
@@ -7,40 +7,37 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/search.php");
 $msg = doLogin();
 ?>
  
- <html>
+<html>
+	<head>
+ 		<title>EventHub</title>
+ 		<!-- Time stamp on css to stop caching so I can actually get work done when making changes -->
+        <link rel="stylesheet" type="text/css" href="css/style.css?<?php echo date('l jS \of F Y h:i:s A'); ?>" media="screen" />
+	 	<script>
+	 		function RemoveText(obj) {   obj.value = ''; } 
+	 	</script>
+    </head>
 
- 	<header>
-
- 	<script>
- 		function RemoveText(obj) {   obj.value = ''; } 
- 	</script>
-
- 	</header>
 	<body>
-
-
-		<!-- Simple login logic here:
-					If not logged in, show the login form
-					If logged in, then welcome user and show profile link
-		-->
-		<?php login_div($msg); ?>
-
-		
-
-		<h1>Welcome to EventHub!</h1>
-
-		
-		<!--<img src="./resources/img/Homepage.jpg" />-->
-		<div id="search-area">
+		<header>
 			<form name="login" id="login" action="index.php" method="POST">
 				<label for="username"/></label>
 				<input type="text" name="search_query" value="" onclick="RemoveText(this);">
 				<input class= "btn" name="search_submit" type="submit" value="Search Events">
 			</form>
+		</header>
+	 	<div id="main_center">
+			<!-- Simple login logic here:
+						If not logged in, show the login form
+						If logged in, then welcome user and show profile link
+			-->
+			<?php login_div($msg); ?>
 
+			<h1>EventHub</h1>
 
-			<div id="search_results">
-				<?php doEventSearch(); ?>
+			<div id="search-area">
+				<div id="search_results">
+					<?php doEventSearch(); ?>
+				</div>
 			</div>
 
 		</div>
