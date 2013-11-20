e637083cf75a5620d495caabd3be513f00c5c964
chris lloyd <cl71@chrislloyd.co>
2013-11-20 18:54:06 -0500

    test invite

--- event.php
+++ event.php
@@ -4,7 +4,10 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/data.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/login.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/search.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/event.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/invite.php");
 $msg = doLogin();
+
+$invite_message = doInvite();
 ?>
  
  <html>
@@ -24,6 +27,17 @@ $msg = doLogin();
 			eventHTML();
 		?>
 
+		<div id="invite">
+			<form name="invite" id="invite" action="invite.php" method="POST">
+				<label for="username"/></label>
+				<input type="text" name="username" value="" size="50">
+				<input class= "btn" type="submit" name="invite_submit" value="Invite user">
+			</form>
+
+			<div id="invite_message">
+				<?php echo $invite_message; ?>
+			</div>
+		</div>
 		
 	</body>
 </html>
---
+++ resources/include/invite.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/data.php");
+
+
+
+
+function doInvite() {
+	$message = "";
+
+	if(isset($_POST['invite_submit'])) {
+
+		echo "invite submit pressed";
+
+		$username = $_POST['username'];
+		$invitee_UID = getUserID($username);
+		$inviter_UID = getActiveUser();
+
+		$EID = $_GET['u'];
+
+		if ($inviter_UID == -1) {
+			$error_message .= "<p>You must be logged in to invite others.</p>"
+		}
+
+		if ($invitee_UID == -1) {
+			$error_message .= "<p>Please check that the user you want to invite exists.</p>"
+		}
+
+		// if no errors, then send the invite
+		if ($error_message == "") {
+			sendInvite($inviter_UID, $invitee_UI, $EID, $msg="");
+			$message = "<p>You successfully invited {$username}!</p>";
+		}
+
+	}
+
+	return $message;
+
+}
+
+?>
