02d1436be997f5daa9a526aa5c7dc60ff21b3c0d
chris lloyd <cl71@chrislloyd.co>
2013-11-20 19:35:11 -0500

    test rsvp

--- resources/include/rsvp.php
+++ resources/include/rsvp.php
@@ -4,15 +4,44 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/data.php");
 
 function doRSVP() {
 
-	$message = "";
+
 
 	if(isset($_POST['rsvp_submit'])) {
 
 		$rsvp = $_POST['decision'];
+		$message = "";
+		$UID = getActiveUser();
+		$EID = $_GET['e'];
 
-		echo $rsvp;
-	}
+	
 
+		if (empty($rsvp)){
+			$message .= "<p>Please select an RSVP option.</p>";
+		}
+
+		if ($UID == -1){
+			$message .= "<p>Please log in to RSVP.</p>";
+		}
+
+ 		if ($message == "") {
+
+
+ 			if($rsvp == "Yes") {
+ 				$message = "<p>RSVP'd! See you there!</p>";
+ 				$rsvp = 2;
+ 				addRSVP($UID, $EID, $rsvp);
+ 			} else if ($rsvp == "Maybe") {
+ 				 	$message = "<p>We hope you make it!</p>";
+ 					$rsvp = 1;
+ 					addRSVP($UID, $EID, $rsvp);
+ 			} else if ($rsvp == 0) {
+ 				 	$message = "<p>If you change your mind, come back and RSVP anytime!</p>";
+ 					$rsvp = 0;
+ 					addRSVP($UID, $EID, $rsvp);
+ 			}
+ }
+
+}
 
 	return $message;
 }
