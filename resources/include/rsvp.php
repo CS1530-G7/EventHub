1cb42b9688d6cdd399998fa90daa8bc2e9349be6
chris lloyd <cl71@chrislloyd.co>
2013-11-20 19:58:49 -0500

    test rsvp

--- resources/include/rsvp.php
+++ resources/include/rsvp.php
@@ -14,7 +14,7 @@ function doRSVP() {
 		$EID = $_GET['e'];
 
 
-		echo getUserRSVP($UID, $EID);
+		//echo getUserRSVP($UID, $EID);
 
 	
 		// check for input errors
@@ -39,25 +39,49 @@ function doRSVP() {
  					$userCurrentRSVP = getUserRSVP($UID, $EID);
  				}
  			}
- 			echo $test;
 
 
- 			if($rsvp == "Yes") {
- 				$message = "<p>RSVP'd! See you there!</p>";
- 				$rsvp = 2;
- 				addRSVP($UID, $EID, $rsvp);
- 			} else if ($rsvp == "Maybe") {
- 				 	$message = "<p>We hope you make it!</p>";
- 					$rsvp = 1;
+ 			// if a user is not already chosen an RSVP option for this event, then do these
+ 			if($userCurrentRSVP == -1) {
+
+
+ 				if($rsvp == "Yes") {
+ 					$message = "<p>RSVP'd! See you there!</p>";
+ 					$rsvp = 2;
  					addRSVP($UID, $EID, $rsvp);
- 			} else if ($rsvp == 0) {
- 				 	$message = "<p>If you change your mind, come back and RSVP anytime!</p>";
- 					$rsvp = 0;
- 					addRSVP($UID, $EID, $rsvp);
+ 				} else if ($rsvp == "Maybe") {
+ 				 		$message = "<p>We hope you make it!</p>";
+ 						$rsvp = 1;
+ 						addRSVP($UID, $EID, $rsvp);
+ 				} else if ($rsvp == 0) {
+ 				 		$message = "<p>If you change your mind, come back and RSVP anytime!</p>";
+ 						$rsvp = 0;
+ 						addRSVP($UID, $EID, $rsvp);
+ 				}
+
+
  			}
- }
+ 			// if the user has already picked something, then do the change function
+ 			else {
 
-}
+ 				if($rsvp == "Yes") {
+ 					$message = "<p>RSVP'd! Glad you decided to go!</p>";
+ 					$rsvp = 2;
+ 					changeRSVP($UID, $EID, $rsvp);
+ 				} else if ($rsvp == "Maybe") {
+ 				 		$message = "<p>Oh, you're maybe going now?</p>";
+ 						$rsvp = 1;
+ 						changeRSVP($UID, $EID, $rsvp);
+ 				} else if ($rsvp == 0) {
+ 				 		$message = "<p>Make up your mind and go!</p>";
+ 						$rsvp = 0;
+ 						changeRSVP($UID, $EID, $rsvp);
+ 				}
+
+
+
+ 			}
+ 	}
 
 	return $message;
 }
