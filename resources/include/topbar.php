119908c9b2fa4db13fa49fdfa4a98b1b3b2bb3f8
drewmcd621 <drewmcd621@bitbucket.org>
2013-11-20 21:08:46 -0500

    Topbar

---
+++ resources/include/topbar.php
@@ -0,0 +1,90 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/data.php");
+
+function doLogin()
+{
+	$error_message = "";
+
+	if(isset($_POST['submit'])) {
+
+			// get variables
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+
+			$userID = login($username, $password);
+
+		 if(empty($username) || empty($password)) {
+
+			$error_message .= "<p>Username and/or password field blank.</p>";
+
+		 }
+		 else if ($userID == -1) {
+
+			$error_message .= "<p>Username and/or password incorrect.</p>";
+
+		 }
+		 else
+		 {
+			 header( "Location:profile.php?u=$userID");
+		 }
+
+	}
+	else if(getActiveUser() == -3)
+	{
+		$error_message .= "<p>Your session has expired, please login again.</p>";
+	}
+	
+	return $error_message;
+	
+}
+
+$Home = FALSE;
+
+print $_SERVER["PHP_SELF"]; 
+
+$msg = doLogin();
+?>
+
+		<div id='header'>
+			<? if(!$Home){?>
+			<div id="search-area">
+				<form name="search" id="search" action="search.php" method="GET">
+					<input type="text" name="q" value="" size="50"><br><br>
+					<input class= "btn" type="submit" value="Search Events">
+				</form>
+			</div>
+			
+			<div id='Home'>
+				<a href='/'>Home</a>
+			</div>
+			<? 
+			} 			
+			?>
+			<div id="login">
+			<?
+			$UID = getActiveUser();
+			if($UID >= 0) 
+			{
+				$user = getUsername($UID);
+				$link = "profile.php?u=$UID";
+			?>
+			<a href='<?print $link; ?>'><? print $user; ?></a> | <a href=\"logout.php\">Logout</a>
+			<?
+			}else{
+			?>
+			<div id="login-form">
+				<form name="login" id="login" action="index.php" method="POST">
+					<input type="text" name="username" value="Username">
+					<input type="password" name="password" value="Password">
+					<input class= "btn" name="submit" type="submit" value="Login">
+				</form>
+				<div id="login-errors">
+					<? print $msg; ?>
+				</div>
+			</div>
+			<div id="sign-up">
+				<p>Not registered? <a href="signup.php">Sign up!</a></p>	
+			</div>
+			<? } ?>
+		</div>
+	</div>
