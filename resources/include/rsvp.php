e3ed567587c257f4a57a9df6a6a786c3cebb0408
chris lloyd <cl71@chrislloyd.co>
2013-11-20 19:26:44 -0500

    test rsvp

--- resources/include/rsvp.php
+++ resources/include/rsvp.php
@@ -2,7 +2,7 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . "/resources/include/data.php");
 
-function doRSVP {
+function doRSVP () {
 
 	$message = "";
 
@@ -13,6 +13,8 @@ function doRSVP {
 		echo $rsvp;
 	}
 
+
+	return $message;
 }
 
 
